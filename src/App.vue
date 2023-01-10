1ed6e39189cabda03055851cd7e518ee66cdbcc9
aao <aungaungoogame2@gmail.com>
2023-01-10 20:32:06 +0630

    nav roter import

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/webtoon">Webtoon</router-link> |
+    <router-link to="/manga">Manga</router-link>
+    
   </nav>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import WebtoonPage from "../views/WebtoonPage.vue";
+import MangaPage from "../views/MangaPage.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: "/webtoon",
+    name: "webtoon",
+    component: WebtoonPage,
+  },
+  {
+    path: "/manga",
+    name: "manga",
+    component: MangaPage,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
