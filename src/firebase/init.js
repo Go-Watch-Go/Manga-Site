524200a510ae2651a82385b5fdd750637e55458c
Reggae Homer <mgthantsinmm21@gmail.com>
2023-01-11 00:49:53 +0630

    adding firebase/to store manga data and use as own db

---
+++ src/firebase/init.js
@@ -0,0 +1,32 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDnAzA1vQZ1faWd8uP2K4tvu-Amx4GhbG4",
+  
+    authDomain: "manga-19e8c.firebaseapp.com",
+  
+    projectId: "manga-19e8c",
+  
+    storageBucket: "manga-19e8c.appspot.com",
+  
+    messagingSenderId: "1070277497667",
+  
+    appId: "1:1070277497667:web:990de1bc56bf30360d95f6"
+  
+  };
+
+  // Initialize Firebase
+
+    const app = initializeApp(firebaseConfig);
+
+  // init firestore service
+
+  const db = getFirestore();
+  export default db;
+  
