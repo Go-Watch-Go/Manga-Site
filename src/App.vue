e4e80fbfb726af02d7a79dd2511542ca5ebb9ad6
Reggae Homer <mgthantsinmm21@gmail.com>
2023-01-16 22:29:39 +0630

    data fetch from firestore

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/webtoon">Webtoon</router-link> |
-    <router-link to="/manga">Manga</router-link>
+    <router-link to="/manga">Manga</router-link> |
+    <router-link to="/detail">Detail</router-link>
     
   </nav>
   <router-view/>
---
+++ src/components/SummaryInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Summary Info</h1>
+  <h3>{{dat.title}}</h3>
+</template>
+
+<script>
+import {ref} from "vue";
+import {doc, getDoc} from 'firebase/firestore';
+import db from '../firebase/init';
+export default {
+ setup(){
+  let dat = ref('');
+  let load = async()=>{
+    let res = await getDoc(doc(db, 'detail','s3PElJbCG0QRs1V9hIxp'))
+
+    if(res.exists()){
+      // console.log(res.data());
+      let storage = res.data();
+      dat = storage;
+      console.log(dat.arthur);
+    }else{
+      console.log("Sorry");
+    }
+  }
+  load();
+  
+  return{load,dat}
+ }
+}
+</script>
+
+<style>
+
+</style>
